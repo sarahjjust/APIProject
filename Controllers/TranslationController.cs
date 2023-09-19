06a44050b4b6329db19308bc395f6a57ab6db11e
sarahjjust <siriusjjust@gmail.com>
2023-09-19 15:56:38 -0400

    Added to API. You can now get random words, or translate the other direction.

--- Controllers/TranslationController.cs
+++ Controllers/TranslationController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
 //using Microsoft.AspNetCore.Routing.Constraints;
 using System.IO;
 
@@ -28,7 +30,7 @@ public class TranslationController : ControllerBase
         return "Hello World!";
     }
 
-    [HttpGet("{text}")]
+    [HttpGet("en/jp/{text}")]
     public string GetJPTranslation(string text) {
         Response.Headers.Add("Access-Control-Allow-Origin", "*");
 
@@ -42,5 +44,109 @@ public class TranslationController : ControllerBase
             line = sr.ReadLine();
         }
         return "Not in dictionary :(";
+    }
+
+    [HttpGet("jp/en/{text}")]
+    public string GetENTranslation(string text) {
+        Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+        StreamReader sr = new StreamReader("./dict.txt");
+        int len = text.Length;
+        string? line = sr.ReadLine();
+        while (line != null) {
+            if (line.Length > len+2 && line.Substring(line.Length - len - 2).Equals(": " + text.ToLower())) {
+                return line.Substring(0, line.Length - len - 2);
+            }
+            line = sr.ReadLine();
+        }
+        return "Not in dictionary :(";
+    }
+
+    [HttpGet("rand/en/{num}")]
+    public string[] GetRandomENWords(int num) {
+        Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+        if (num <= 0) {
+            return new string[0];
+        }
+
+        // Get size num array of random numbers between 0 and 999 with no repeats.
+        Random rand = new Random();
+        List<int> choiceList = new List<int>();
+        for (int i = 0; i < num; i++) {
+            int nextChoice = rand.Next(1000);
+            while (choiceList.Contains(nextChoice)) {
+                nextChoice = rand.Next(1000);
+            }
+            choiceList.Add(nextChoice);
+        }
+        int[] choices = choiceList.ToArray();
+        Array.Sort(choices);
+
+        // For each number in the array, get that entry in the dictionary.
+        StreamReader sr = new StreamReader("./dict.txt");
+        string[] result = new string[num];
+        int currIndex = 0;
+        int curr = choices[currIndex];
+        for (int i = 0; i < 1000; i++) {
+            if (i == curr) {
+                string? line = sr.ReadLine();
+                int demarcIndex = line.IndexOf(":");
+                result[currIndex] = line.Substring(0, demarcIndex);
+                currIndex++;
+                if (currIndex >= num) break;
+                curr = choices[currIndex];
+            } else {
+                sr.ReadLine();
+            }
+        }
+
+        Debug.Assert(result.Length == num);
+
+        return result;
+    }
+
+    [HttpGet("rand/jp/{num}")]
+    public string[] GetRandomJPWords(int num) {
+        Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+        if (num <= 0) {
+            return new string[0];
+        }
+
+        // Get size num array of random numbers between 0 and 999 with no repeats.
+        Random rand = new Random();
+        List<int> choiceList = new List<int>();
+        for (int i = 0; i < num; i++) {
+            int nextChoice = rand.Next(1000);
+            while (choiceList.Contains(nextChoice)) {
+                nextChoice = rand.Next(1000);
+            }
+            choiceList.Add(nextChoice);
+        }
+        int[] choices = choiceList.ToArray();
+        Array.Sort(choices);
+
+        // For each number in the array, get that entry in the dictionary.
+        StreamReader sr = new StreamReader("./dict.txt");
+        string[] result = new string[num];
+        int currIndex = 0;
+        int curr = choices[currIndex];
+        for (int i = 0; i < 1000; i++) {
+            if (i == curr) {
+                string? line = sr.ReadLine();
+                int demarcIndex = line.IndexOf(":");
+                result[currIndex] = line.Substring(demarcIndex+1);
+                currIndex++;
+                if (currIndex >= num) break;
+                curr = choices[currIndex];
+            } else {
+                sr.ReadLine();
+            }
+        }
+
+        Debug.Assert(result.Length == num);
+
+        return result;
     }
 }
